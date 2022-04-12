25ffeedf69aa42536222d6179a8cdd0b2e2a19bb
r-extream <venera_ok@mail.ru>
2022-04-12 08:35:57 +0300

    8th lesson

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from  django.test import TestCase
+from mixer.backend.django import mixer
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import CustomUser
+from todo.models import Project
+
+
+class TestProject(APITestCase):
+    def test_creating_project(self):
+        CustomUser.objects.create_superuser('admin', 'admin@mail.local', 'Qwerty123456')
+        project = mixer.blend(Project)
+        self.client.login(username='admin@mail.local', password='Qwerty123456')
+        result = self.client.get('/api/projects/1/')
+        self.assertEqual(result.data['name'], project.name)
